c4a240cccd52c82159669d927c7d4c84fffcbd0e
Alejandro Medrano <105515338+rezself@users.noreply.github.com>
2022-06-24 15:47:15 -0500

    feat: added api project

---
+++ README.md
@@ -0,0 +1,1 @@
+# bonds-value-api-json-server
---
+++ db.js
@@ -0,0 +1,32 @@
+const items = require("./data/users");
+const tags = require("./data/bonds");
+
+module.exports = {
+  items,
+  tags
+}
+
+
+// const items = require("./data/items");
+
+// module.exports = () => {
+
+//   var tags = items.map((i) => i.tags);
+//   tags = [].concat.apply([], tags);
+//   tags = [...new Set(tags)];
+
+ 
+
+//   var brands = items.map((i) => i.manufacturer);
+//   brands = [...new Set(brands)];
+
+//   var itemTypes = items.map((i) => i.itemType);
+//   itemTypes = [...new Set(itemTypes)];
+
+//   return {
+//     items,
+//     tags,
+//     brands,
+//     itemTypes,
+//   };
+// };
